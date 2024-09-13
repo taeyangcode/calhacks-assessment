1f5ead9ba8edbfabb6eef32c9e23ec96ab1f2cf3
Corey Mostero <mostero@berkeley.edu>
2024-09-12 21:14:03 -0700

    api: base setup

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{web, App, HttpServer};
+use firestore::{FirestoreDb, FirestoreDbOptions};
+
+async fn signup() -> actix_web::Result<String> {
+    todo!()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let _ = dotenvy::from_path("./env.dev");
+
+    let database_project_id = dotenvy::var("FIREBASE_SERVICE_ACCOUNT_PATH").unwrap();
+    let database_service_account_path = dotenvy::var("SERVICE_ACCOUNT_PATH").unwrap();
+    let port = dotenvy::var("BACKEND_PORT")
+        .unwrap()
+        .parse::<u16>()
+        .unwrap();
+
+    let database = FirestoreDb::with_options_service_account_key_file(
+        FirestoreDbOptions::new(database_project_id),
+        database_service_account_path.into(),
+    )
+    .await
+    .unwrap();
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(web::Data::new(database.clone()))
+            .route("/api/signup", web::post().to(signup))
+    })
+    .bind(("127.0.0.1", port))?
+    .run()
+    .await
 }
